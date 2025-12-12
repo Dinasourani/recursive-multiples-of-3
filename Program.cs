538c7705b32c02761a0014f25e0a84c696489cb0
Dina sourani <dyxz8613@gmail.com>
2025-12-12 13:44:23 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void MultiplesOf3(int n)
+        {
+
+            if (n <= 0)
+                return;
+
+
+            if (n % 3 != 0)
+            {
+                MultiplesOf3(n - 1);
+            }
+            else
+            {
+
+                MultiplesOf3(n - 3);
+
+
+                Console.WriteLine(n);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("عدد N را وارد کنید: ");
+            int N = int.Parse(Console.ReadLine());
+
+            MultiplesOf3(N - 1);
+            Console.ReadKey();
+        }
+    }
+}
